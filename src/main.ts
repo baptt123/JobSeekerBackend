46cc886560db52e9c3c382809d4730add975b31c
Thanhtan101103 <161003191+baptt123@users.noreply.github.com>
2025-10-24 07:49:13 +0700

    feat: enhance main application setup with view engine and static assets configuration

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,21 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './exception/global-exception.filter';
 import { config } from 'dotenv';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express'; // ⚡ thêm dòng này
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); // thư mục chứa HTML template
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
   app.useGlobalFilters(new GlobalExceptionFilter());
-
+  app.setViewEngine('hbs');
+  app.useStaticAssets(join(__dirname, '..', 'public')); // nơi chứa CSS, ảnh, JS tĩnh
   await app.listen(3000);
   console.log('Server running on http://localhost:3000');
 }
+
 bootstrap();
